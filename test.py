57d3bdecf56896e093b5e7c1c42681223ef66708
Sheamus Burns <sheamusburns@gmail.com>
2017-11-11 19:21:45 -0500

    add notes

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# pip3 install requests if you don't have it. 
+
+
 import os
 import requests
 from datetime import datetime
@@ -9,8 +13,8 @@ dir_path = os.getcwd() #figure out which directory python is running in
 print(dir_path)
 os.chdir('/users/sburns/code/mblocator/')
 
-dir_path = os.getcwd() #figure out which directory python is running in
-print(dir_path)
+# dir_path = os.getcwd() #figure out which directory python is running in
+# print(dir_path)
 
 r = requests.get('https://api.ipify.org?format=json');
 
