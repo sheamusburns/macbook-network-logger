0ed677e3c0b550bbf8b0704ac7b06689e86df3f5
Sheamus Burns <sheamusburns@gmail.com>
2017-11-11 19:16:04 -0500

    currently logging ip address whenever SystemConfiguration folder changes (ie. Wifi is turned on and off or network is switched)

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print("success")
+#!/usr/bin/env python
+import os
+import requests
+from datetime import datetime
+
+now = datetime.now()
+
+dir_path = os.getcwd() #figure out which directory python is running in
+print(dir_path)
+os.chdir('/users/sburns/code/mblocator/')
+
+dir_path = os.getcwd() #figure out which directory python is running in
+print(dir_path)
+
+r = requests.get('https://api.ipify.org?format=json');
+
+
+f = open('/Users/sburns/Code/mblocator/log.txt','a')
+jsonIP = r.json()
+f.write('\n' + str(now) + ' - IP: ' + jsonIP['ip'])
+f.close()
