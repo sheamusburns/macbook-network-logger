1b9a637c49a29a813ff76df509303e193959a371
Sheamus Burns <sheamusburns@gmail.com>
2017-11-12 23:42:02 -0500

    refactor, get device info, add logging to firebase'

--- test.py
+++ test.py
@@ -7,7 +7,10 @@ import os
 import requests
 from datetime import datetime
 import subprocess
+import json
+import re
 
+db_address = config.db_address
 homeIP = config.homeIP
 project_path = config.project_path
 log_filename = config.log_filename
@@ -20,24 +23,45 @@ curr_dir_path = os.getcwd()
 if curr_dir_path != project_path:
 	os.chdir(project_path)
 
+hardware = subprocess.run(['system_profiler', 'SPHardwareDataType'], stdout=subprocess.PIPE)
+hardware = hardware.stdout.decode('utf-8')
+serial = re.findall('Serial\sNumber\s\(system\):\s(.+)', hardware)[0]
+db_path = serial
+
 curr_wifi_request = subprocess.run(get_wifi_on_mac_args, stdout=subprocess.PIPE)
 curr_wifi_info = curr_wifi_request.stdout.decode('utf-8')
+print(curr_wifi_info)
+
 
 r = requests.get('https://api.ipify.org?format=json');
 jsonIP = r.json()
+ssid = re.findall('\sSSID:\s(.+)', curr_wifi_info)[0]
+bssid = re.findall('\sBSSID:\s(.+)', curr_wifi_info)[0]
+
+r1 = requests.get('http://ip-api.com/json')
+json_wrong_ip = r1.json()
+lat = json_wrong_ip['lat']
+lon = json_wrong_ip['lon']
+city = json_wrong_ip['city']
+
+
 
 f = open(project_path + log_filename,'a')
 f.write(str(now) + '\nReturned IP: ' + jsonIP['ip'] + '\n')
-f.write(curr_wifi_info)
 
 if jsonIP['ip'] != homeIP:
-	f.write('Status: Wrong IP\n')
-	r1 = requests.get('http://ip-api.com/json')
-	json_wrong_ip = r1.json()
-	f.write(str(json_wrong_ip) + '\n\n')
+	status = 'BAD IP'
+	f.write('Status: BAD IP\n')
 	alert = subprocess.run(["osascript", "-e", "tell app \"System Events\" to display dialog" + warning_string])
 else:
+	status = 'OK'
 	f.write('Status: OK\n\n')
-
+f.write(curr_wifi_info)
+f.write(str(json_wrong_ip) + '\n\n')
 f.close()
+
+
+
+j = json.dumps({'time':str(now), 'status': status, 'ip':jsonIP['ip'], 'ssid': ssid, 'bssid': bssid, 'city': city, 'lat': lat, 'lon': lon, 'serial': serial})
+l = requests.post(db_address + db_path + '.json', data=j)
 
