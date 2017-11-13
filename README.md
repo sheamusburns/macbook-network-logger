26f7c39bc5cd43437b5ffccbd8f373ff732b6212
Sheamus Burns <sheamusburns@gmail.com>
2017-11-12 23:49:54 -0500

    update description and config

---
+++ README.md
@@ -0,0 +1,3 @@
+# macbook locator
+
+##When your Mac connects to a new network, this tool checks if it's a default network that you set, and if it isn't it logs the Wifi, IP, Hardware, and Location (from IP) info to a firebase db. 
--- empty_config.py
+++ empty_config.py
@@ -2,3 +2,4 @@ project_path = '/project/folder/path/'
 log_filename = 'log.txt'
 warning_string = "\"You have connected to an unverified network. Please reconsider your deviance. Violators will be annihilated. Your actions have been logged.\""
 homeIP = '00.00.00.00'
+db_address = #firebase url 
